03e33436e5be885dcb832afe810c63e8b5fb8a02
JRonca <jrfr21112000@gmail.com>
2024-05-30 13:03:11 -0300

    1036 - Formula de Bhaskara

---
+++ 1-Iniciante/P-1036-Formula_de_Bhaskara.cpp
@@ -0,0 +1,26 @@
+// 1036 - Fórmula de Bhaskara
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a,b,c;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+    
+    double delta = (b*b)-(4*a*c);
+
+    if(!a || delta < 0){
+        printf("Impossivel calcular\n");
+        return 0;
+    }
+
+    double raiz = sqrt(delta);
+    double BhaskaraX1 = ((-1*b)+raiz)/(2*a);
+    double BhaskaraX2 = ((-1*b)-raiz)/(2*a);
+
+    printf("R1 = %.5lf\n", BhaskaraX1);
+    printf("R2 = %.5lf\n", BhaskaraX2);
+    
+    return 0;
+}
